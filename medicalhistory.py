e60aa22fc61be4c4a3e4c52901d800ea691af3e7
Suhrid Sadman Abrar <suhridabrarbd@outlook.com>
2023-02-05 16:42:19 +0530

    unconflited

--- medicalhistory.py
+++ medicalhistory.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from add_bg_from_local import *
 import streamlit as st
 import csv
@@ -54,3 +55,34 @@ with col4:
 
 st.subheader("Thank you for writing your details! To continue, please click on the 'Complete' button below! :point_down:")
 complete = st.button("Complete!", key="beta23", help="You will go to the next section after this on click!")
+=======
+from add_bg_from_local import *
+import streamlit as st
+import csv
+
+add_bg_from_local("vax8.jpg")
+st.title("Welcome to the Medical History section! :wave: ")
+st.subheader("Here, we want to know your medical condition before recommending any vaccination. In Sri Lanka :flag-lk:, we are dealing with a variety of diseases that you can be immuned from through vaccines. As adults, these diseases can be life-threatning and lead to a unhealthy life or death. ")
+st.header("With no further ado, let's begin by ticking any of the following below based on your medical history! :point_down:")
+
+col1, col2 = st.columns(2)
+with col1:
+    st.checkbox("Any reported allergies after taking a vaccine")
+    st.checkbox("Asthma")
+    st.checkbox("Hypertension")
+    st.checkbox("Liver Disease")
+    st.checkbox("Diabetes")
+    st.checkbox("Allergies")
+    st.checkbox("Pregnant")
+    st.checkbox("Under blood transfusion")
+with col2:
+    st.checkbox("Men having sex with men (MSM)")
+    st.checkbox("Immunocompromised")
+    st.checkbox("Haemophilia")
+    st.checkbox("Tubercolosis (TB)")
+    st.checkbox("Any sexually transmitted disease (STDs)")
+    st.checkbox("Sexually active")
+    st.checkbox("GBS (Guillain-Barré Syndrome)")
+    st.checkbox("Alcoholic")
+    st.checkbox("Smoker")
+>>>>>>> 0fbd1aec32a6c6a23fc5bfc037d9e3b6b4dd94c8
